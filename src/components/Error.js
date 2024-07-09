44710b197c4c163562b23316910143c68b08ad4b
Abhimanyu Ahuja <abhimanyuahuja12@gmail.com>
2024-07-10 00:22:51 +0530

    Added Error Element

---
+++ src/components/Error.js
@@ -0,0 +1,17 @@
+import { useRouteError } from "react-router-dom";
+import { errorImage } from "../utils/constants";
+const Error=()=>{
+
+    const err =useRouteError();
+    console.log(err);
+    return(
+        <div>
+            {/* /* <h1>oops !!</h1>
+            <h2>{err.status}: {err.statusText}</h2> */ }
+            <img src={errorImage} alt={err.status} />
+        </div>
+    )
+}
+export default Error;
+
+
